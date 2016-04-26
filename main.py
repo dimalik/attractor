db49a2e60d44b4a6ee325e4c3eb16ca5f9459103
Dimitris Alikaniotis <dim.alikaniotis@gmail.com>
2016-04-26 11:52:56 +0100

    rewrote plotting

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import os
 import logging
 
+import matplotlib.pyplot as plt
+
 from config import options
 from scripts import Attractor, RandomInput, McRaeBoisvertExperiment, priming
 
 logger = logging.getLogger(__name__)
 
 
-def main():
+def getCurves():
     mcrae = McRaeBoisvertExperiment(
         os.path.join(options['data_path'], 'mcrae.pkl'),
         os.path.join(options['data_path'], 'words.pkl'))
-    N_WORDS = len(mcrae.include)
+    nb_words = len(mcrae.include)
 
-    X = RandomInput(N_WORDS, options['input_dims'],
+    X = RandomInput(nb_words, options['input_dims'],
                     prob=options['binomial_probability'])
 
     attractor = Attractor(options['input_dims'],
-                          mcrae.experiment.matrix.shape[1], batch_size=N_WORDS)
+                          mcrae.experiment.matrix.shape[1],
+                          batch_size=nb_words)
+    attractor.fit(X.matrix, mcrae.experiment.matrix,
+                  nb_epochs=options['epochs'])
 
-    attractor.fit(X.matrix, mcrae.experiment.matrix, nb_epochs=5)
-    prime_res = priming(X.matrix, mcrae.experiment.dicts[0], mcrae.df,
-                        attractor.get_priming)
-    return prime_res
+    return priming(X.matrix, mcrae.experiment.dicts[0],
+                        mcrae.df, attractor.get_priming)
 
-# rewrite that
-X = main()
-import matplotlib.pyplot as plt
 
-plt.plot(X['Similar'], label='Similar')
-plt.plot(X['Dissimilar'], label='Dissimilar')
+def plotResults(res):
+    plt.plot(res['Similar'], label='Similar')
+    plt.plot(res['Dissimilar'], label='Dissimilar')
+    plt.legend(loc='upper left')
+    plt.show()
 
-plt.legend('upper left')
-plt.show()
+if __name__ == '__main__':
+    plotResults(getCurves())
+    
